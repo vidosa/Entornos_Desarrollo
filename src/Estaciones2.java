5ea4a8330acb2f97eb406371d65712bcdf9999f9
vicentdocedasanchis <enricvds@gmail.com>
2017-11-25 19:35:32 +0100

    añadido estaciones2

---
+++ src/Estaciones2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Estaciones2 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Escribe un número entre 1 y 12");
+		num = entrada.nextInt();
+
+		if (num >= 1 && num <= 12) {
+			switch (num) {
+			case 3:
+			case 4:
+			case 5:
+				System.out.println("La estación es primavera");
+				break;
+			case 6:
+			case 7:
+			case 8:
+				System.out.println("La estación es verano");
+				break;
+			case 9:
+			case 10:
+				System.out.println("La estación es otoño");
+				break;
+			case 11:
+			case 12:
+			case 1:
+			case 2:
+				System.out.println("La estación es invierno");
+				break;
+			}
+		} else {
+			System.out.println(num + " no es un número correcto");
+		}
+
+	}
+
+	}
+
+
